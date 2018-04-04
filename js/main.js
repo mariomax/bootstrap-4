3cf3acdcdd52e45a28b6f138de72f39a61e7c728
Mario Lopriore <mario@Marios-MBP.domain>
2018-04-04 15:56:07 -0700

    Completed navigation and some larger format styling

--- js/main.js
+++ js/main.js
@@ -4,8 +4,17 @@ $(document).ready(function () {
     $('.header').height($(window).height());
 });
 
-$(".navbar a").click(function () {
+$(".navbar-nav a").click(function () {
     $("body,html").animate({
         scrollTop:$("#" + $(this).data('value')).offset().top
     }, 1000)
 });
+
+function classToggle() {
+    const navs = document.querySelectorAll('.navbar-items')
+    
+    navs.forEach(nav => nav.classList.toggle('navbar-toggleShow'));
+  }
+  document.querySelector('.navbar-link-toggle')
+    .addEventListener('click', classToggle);
+
