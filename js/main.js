adea8cc869c8111ac656db733d3654a00c457cae
Mario Lopriore <mario@Marios-MBP.domain>
2018-04-06 16:07:04 -0700

    Created resposive scrollTop offset and closing menu

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 /* global $ */
 
+// Original code from freeCodeCamp landing page tutorial @ https://medium.freecodecamp.org/learn-bootstrap-4-in-30-minute-by-building-a-landing-page-website-guide-for-beginners-f64e03833f33
 $(document).ready(function () {
     $('.header').height($(window).height());
 });
 
 $(".navbar-nav a").click(function () {
+    if ($(window).width() < 768) {
+        $("body,html").animate({
+            scrollTop:$("#" + $(this).data('value')).offset().top - 200 }, 1000)
+    }
+    else 
     $("body,html").animate({
-        scrollTop:$("#" + $(this).data('value')).offset().top
-    }, 1000)
+        scrollTop:$("#" + $(this).data('value')).offset().top }, 1000)
 });
 
-function classToggle() {
-    const navs = document.querySelectorAll('.navbar-items')
-    
-    navs.forEach(nav => nav.classList.toggle('navbar-toggleShow'));
-  }
-  document.querySelector('.navbar-link-toggle')
-    .addEventListener('click', classToggle);
-
+// Edited from codepen @ http://jsfiddle.net/rcdtest/HDQNs/
+$(".navbar-link-toggle, ul li a").click(function () {
+    $(".navbar-items").toggleClass("open");
+});
